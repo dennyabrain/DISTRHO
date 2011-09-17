0ae5c31557bae27195b48a151521d683778730a9
falkTX <falktx@gmail.com>
2011-09-17 14:51:42 +0100

    Initial LV2 build test

---
+++ ports/eqinox/LV2/premake.lua
@@ -0,0 +1,48 @@
+
+project.name = "EQinox"
+project.bindir = "../../../bin/EQinox.lv2"
+project.libdir = project.bindir
+project.configs = { "Release", "Debug" }
+
+package = newpackage()
+package.name = project.name
+package.kind = "dll"
+package.language = "c++"
+package.linkflags = { "static-runtime" }
+
+package.config["Release"].target       = project.name
+package.config["Release"].objdir       = "intermediate/Release"
+package.config["Release"].buildoptions = { "-O2 -march=native -msse -ffast-math -fvisibility=hidden -static" }
+package.config["Release"].links        = { "freetype", "pthread", "rt", "X11", "Xext", "juce-plugin-153" }
+
+package.config["Debug"].target         = project.name .. "_debug"
+package.config["Debug"].objdir         = "intermediate/Debug"
+package.config["Debug"].buildoptions   = { "-O0 -ggdb -static" }
+package.config["Debug"].links          = { "freetype", "pthread", "rt", "X11", "Xext", "juce-plugin-153_debug" }
+
+-- TODO: Check for OS
+package.config["Release"].defines      = { "LINUX=1", "NDEBUG=1" };
+package.config["Debug"].defines        = { "LINUX=1", "DEBUG=1", "_DEBUG=1" };
+
+package.includepaths = {
+    "../source",
+    "/usr/include",
+    "/usr/include/freetype2",
+    "../../../libs/juce-153/plugin",
+    "../../../libs/juce-153/source"
+}
+
+package.libpaths = {
+    "/usr/X11R6/lib",
+    "/usr/lib",
+    "../../../libs"
+}
+
+package.files = {
+    matchfiles (
+        "../source/*.cpp",
+        "../source/Components/*.cpp",
+        "../source/Filters/*.cpp",
+        "../../../libs/juce-lv2/juce_LV2_Wrapper.cpp"
+    )
+}
