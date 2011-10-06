356e8dfcb7b902baa1ed2d1976b99e6a2eb5022d
falkTX <falktx@gmail.com>
2011-10-06 14:04:50 +0100

    Added bitmangler

--- ports/bitmangler/VST/premake.lua
+++ ports/bitmangler/VST/premake.lua
@@ -1,5 +1,5 @@
 
-project.name = "juce_pitcher"
+project.name = "bitmangler"
 project.bindir = "../../../bin"
 project.libdir = project.bindir
 project.configs = { "Release", "Debug" }
@@ -13,20 +13,19 @@ package.linkflags = { "static-runtime" }
 package.config["Release"].target       = project.name
 package.config["Release"].objdir       = "intermediate/Release"
 package.config["Release"].buildoptions = { "-O2 -march=native -msse -ffast-math -fvisibility=hidden -static" }
-package.config["Release"].links        = { "freetype", "pthread", "dl", "m", "rt", "X11", "Xext", "juce-plugin-153" }
+package.config["Release"].links        = { "freetype", "pthread", "dl", "rt", "X11", "Xext", "juce-plugin-153" }
 
 package.config["Debug"].target         = project.name .. "_debug"
 package.config["Debug"].objdir         = "intermediate/Debug"
 package.config["Debug"].buildoptions   = { "-O0 -ggdb -static" }
-package.config["Debug"].links          = { "freetype", "pthread", "dl", "m", "rt", "X11", "Xext", "juce-plugin-153_debug" }
+package.config["Debug"].links          = { "freetype", "pthread", "dl", "rt", "X11", "Xext", "juce-plugin-153_debug" }
 
 -- TODO: Check for OS
-package.config["Release"].defines      = { "LINUX=1", "NDEBUG=1", "JUCE_USE_VSTSDK_2_4=1", "JucePlugin_Build_VST=1", "JucePlugin_Build_AU=0", "JucePlugin_Build_RTAS=0", "JucePlugin_Build_Standalone=0" };
-package.config["Debug"].defines        = { "LINUX=1", "DEBUG=1", "_DEBUG=1", "JUCE_USE_VSTSDK_2_4=1", "JucePlugin_Build_VST=1", "JucePlugin_Build_AU=0", "JucePlugin_Build_RTAS=0", "JucePlugin_Build_Standalone=0" };
+package.config["Release"].defines      = { "LINUX=1", "NDEBUG=1", "JUCE_USE_VSTSDK_2_4=1", "JucePlugin_Build_VST=1", "JucePlugin_Build_AU=0", "JucePlugin_Build_RTAS=0" };
+package.config["Debug"].defines        = { "LINUX=1", "DEBUG=1", "_DEBUG=1", "JUCE_USE_VSTSDK_2_4=1", "JucePlugin_Build_VST=1", "JucePlugin_Build_AU=0", "JucePlugin_Build_RTAS=0" };
 
 package.includepaths = {
     "../source",
-    "../source/soundtouch",
     "/usr/include",
     "/usr/include/freetype2",
     "../../../libs/juce-153/plugin",
@@ -43,15 +42,6 @@ package.libpaths = {
 package.files = {
     matchfiles (
         "../source/*.cpp",
-	"../source/soundtouch/AAFilter.cpp",
-	"../source/soundtouch/FIFOSampleBuffer.cpp",
-	"../source/soundtouch/FIRFilter.cpp",
-	"../source/soundtouch/RateTransposer.cpp",
-	"../source/soundtouch/SoundTouch.cpp",
-	"../source/soundtouch/TDStretch.cpp",
-	"../source/soundtouch/cpu_detect_x86_gcc.cpp",
-	"../source/soundtouch/mmx_optimized.cpp",
-	"../source/soundtouch/sse_optimized.cpp",
         "../../../libs/juce-153/source/src/audio/plugin_client/VST/juce_VST_Wrapper.cpp"
     )
 }
