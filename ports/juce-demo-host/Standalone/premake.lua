b3c9701231105fde09ebfc27b842496235ddbac5
falkTX <falktx@gmail.com>
2011-12-06 14:24:18 +0000

    Add juce-demo-host

---
+++ ports/juce-demo-host/Standalone/premake.lua
@@ -0,0 +1,56 @@
+
+project.name = "JuceDemoHost"
+project.bindir = "../../../bin"
+project.libdir = project.bindir
+project.configs = { "Release", "Debug" }
+
+package = newpackage()
+package.name = project.name
+package.kind = "exe"
+package.language = "c++"
+
+package.defines    = { "JUCE_PLUGINHOST_VST=1", "JUCE_PLUGINHOST_AU=1" };
+package.linkflags  = { "no-symbols", "static-runtime" }
+package.links      = { "freetype", "pthread", "asound", "dl", "rt", "X11", "Xext" }
+
+package.config["Release"].target       = project.name
+package.config["Release"].objdir       = "intermediate/Release"
+package.config["Release"].defines      = { "NDEBUG=1" };
+package.config["Release"].buildflags   = { "no-symbols", "optimize-speed" }
+package.config["Release"].links        = { "juce-standalone-vst-153" }
+
+package.config["Debug"].target         = project.name .. "_debug"
+package.config["Debug"].objdir         = "intermediate/Debug"
+package.config["Debug"].defines        = { "DEBUG=1", "_DEBUG=1" };
+package.config["Debug"].links          = { "juce-standalone-vst-153_debug" }
+
+if (windows) then
+  package.defines = { package.defines, "WINDOWS=1" };
+else
+  package.config["Release"].buildoptions = { "-O2 -mtune=generic -msse -ffast-math -fomit-frame-pointer -fvisibility=hidden -fPIC" }
+--   package.config["Release"].linkoptions  = { "-Wl,-O1 -Wl,--as-needed" }
+  package.config["Debug"].buildoptions   = { "-O0 -ggdb -fPIC" }
+  if (macosx) then
+    package.defines = { package.defines, "MAC=1" };
+  else
+    package.defines = { package.defines, "LINUX=1" };
+    package.buildoptions = { "`pkg-config --cflags freetype2`" }
+    package.linkoptions  = { "`pkg-config --libs freetype2`" }
+  end
+end
+
+package.includepaths = {
+    "../source",
+    "../../../libs/juce-153/standalone-vst",
+    "../../../libs/juce-153/source"
+}
+
+package.libpaths = {
+    "../../../libs"
+}
+
+package.files = {
+    matchfiles (
+        "../source/*.cpp"
+    )
+}
