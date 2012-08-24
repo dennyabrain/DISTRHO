d63d7f4bdde7f0cee6864a76b77c6586e4bb36d6
falkTX <falktx@gmail.com>
2012-08-24 06:37:43 +0100

    Add juce-plugin-host to ports

---
+++ ports/juce-plugin-host/Standalone/premake.lua
@@ -0,0 +1,57 @@
+
+project.name    = "JucePluginHost"
+project.bindir  = "../../../bin/standalone"
+project.libdir  = project.bindir
+project.configs = { "Release", "Debug" }
+
+package = newpackage()
+package.name = project.name
+package.kind = "exe"
+package.language = "c++"
+
+package.defines   = { "JucePlugin_Build_Standalone=1", "JucePlugin_Build_Host=1" }
+package.linkflags = { "no-symbols", "static-runtime" }
+
+package.config["Release"].target       = project.name
+package.config["Release"].objdir       = "intermediate/Release"
+package.config["Release"].defines      = { "NDEBUG=1" }
+package.config["Release"].buildflags   = { "no-symbols", "optimize-speed" }
+package.config["Release"].buildoptions = { "-O2 -ffast-math -fomit-frame-pointer -fvisibility=hidden -mtune=generic -msse -mfpmath=sse" }
+package.config["Release"].links        = { "juce-core", "juce-audio-basics", "juce-audio-devices_full", "juce-audio-formats", "juce-audio-processors_full", "juce-audio-utils", "juce-data-structures", "juce-events", "juce-graphics", "juce-gui-basics", "juce-gui-extra" }
+
+package.config["Debug"].target         = project.name .. "_debug"
+package.config["Debug"].objdir         = "intermediate/Debug"
+package.config["Debug"].defines        = { "DEBUG=1", "_DEBUG=1" }
+package.config["Debug"].links          = { "juce-core_debug", "juce-audio-basics_debug", "juce-audio-devices_full_debug", "juce-audio-formats_debug", "juce-audio-processors_full_debug", "juce-audio-utils_debug", "juce-data-structures_debug", "juce-events_debug", "juce-graphics_debug", "juce-gui-basics_debug", "juce-gui-extra_debug" }
+package.config["Debug"].buildoptions   = { "-O0 -ggdb" }
+
+if (windows) then
+  package.defines      = { package.defines, "WINDOWS=1" }
+  package.buildoptions = { "-fpermissive -std=c++0x" }
+  package.links        = { "gdi32", "imm32", "ole32", "shlwapi", "uuid", "version", "winmm", "wininet", "ws2_32" }
+  package.linkoptions  = { "-static -mwindows" }
+elseif (macosx) then
+  package.defines      = { package.defines, "MAC=1" }
+  package.buildoptions = { "-ObjC++" }
+  package.linkoptions  = { "-framework AudioToolbox -framework Cocoa -framework CoreAudio -framework CoreMidi -framework QuartzCore" }
+else
+  package.defines      = { package.defines, "LINUX=1" }
+  package.links        = { "freetype", "pthread", "asound", "dl", "rt", "X11", "Xext" }
+  package.buildoptions = { "-std=c++0x" }
+  package.linkoptions  = { "`pkg-config --libs freetype2`" }
+end
+
+package.includepaths = {
+  "../source",
+  "../../../libs/juce-2.0/source"
+}
+
+package.libpaths = {
+  "../../../libs"
+}
+
+package.files = {
+  matchfiles (
+    "../source/*.cpp"
+  )
+}
---
+++ ports/juce-plugin-host/source/AppConfig.h
@@ -0,0 +1,32 @@
+/*
+
+    IMPORTANT! This file is auto-generated each time you save your
+    project - if you alter its contents, your changes may be overwritten!
+
+    There's a section below where you can add your own custom code safely, and the
+    Introjucer will preserve the contents of that block, but the best way to change
+    any of these definitions is by using the Introjucer's project settings.
+
+    Any commented-out settings will assume their default values.
+
+*/
+
+#ifndef __JUCE_APPCONFIG_NTE0XB0IJ__
+#define __JUCE_APPCONFIG_NTE0XB0IJ__
+
+#include "../build-core/AppConfig.h"
+#include "../build-audio-basics/AppConfig.h"
+#include "../build-audio-formats/AppConfig.h"
+#include "../build-audio-processors_full/AppConfig.h"
+#include "../build-audio-utils/AppConfig.h"
+#include "../build-data-structures/AppConfig.h"
+#include "../build-events/AppConfig.h"
+#include "../build-graphics/AppConfig.h"
+#include "../build-gui-basics/AppConfig.h"
+//#include "../build-gui-extra/AppConfig.h"
+//#include "../build-opengl/AppConfig.h"
+//#include "../build-video/AppConfig.h"
+
+#include "../build-audio-devices_full/AppConfig.h"
+
+#endif  // __JUCE_APPCONFIG_NTE0XB0IJ__
