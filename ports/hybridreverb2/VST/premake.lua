1fb84af8025a381829ef5ae5ab0bee015143bbb2
falkTX <falktx@gmail.com>
2012-09-08 17:27:55 +0100

    Update vst makefiles, enable them

--- ports/hybridreverb2/VST/premake.lua
+++ ports/hybridreverb2/VST/premake.lua
@@ -13,33 +13,35 @@ package.targetprefix = ""
 package.defines   = { "JucePlugin_Build_AU=0", "JucePlugin_Build_LV2=0", "JucePlugin_Build_RTAS=0", "JucePlugin_Build_VST=1", "JucePlugin_Build_Standalone=0" }
 package.linkflags = { "no-symbols", "static-runtime" }
 
-package.config["Release"].target     = project.name
-package.config["Release"].objdir     = "intermediate/Release"
-package.config["Release"].defines    = { "NDEBUG=1" }
-package.config["Release"].buildflags = { "no-symbols", "optimize-speed" }
-package.config["Release"].links      = { "juce-core", "juce-audio-basics", "juce-audio-devices", "juce-audio-formats", "juce-audio-processors", "juce-audio-utils", "juce-data-structures", "juce-events", "juce-graphics", "juce-gui-basics" }
+package.buildoptions = { "`pkg-config --cflags fftw3f`" }
+package.linkoptions  = { "`pkg-config --libs fftw3f`" }
 
-package.config["Debug"].target       = project.name .. "_debug"
-package.config["Debug"].objdir       = "intermediate/Debug"
-package.config["Debug"].defines      = { "DEBUG=1", "_DEBUG=1" }
-package.config["Debug"].links        = { "juce-core_debug", "juce-audio-basics_debug", "juce-audio-devices_debug", "juce-audio-formats_debug", "juce-audio-processors_debug", "juce-audio-utils_debug", "juce-data-structures_debug", "juce-events_debug", "juce-graphics_debug", "juce-gui-basics_debug" }
+package.config["Release"].target       = project.name
+package.config["Release"].objdir       = "intermediate/Release"
+package.config["Release"].defines      = { "NDEBUG=1" }
+package.config["Release"].buildflags   = { "no-symbols", "optimize-speed" }
+package.config["Release"].buildoptions = { "-O2 -ffast-math -fomit-frame-pointer -fvisibility=hidden -mtune=generic -msse -mfpmath=sse" }
+package.config["Release"].links        = { "juce-core", "juce-audio-basics", "juce-audio-devices", "juce-audio-formats", "juce-audio-processors", "juce-audio-utils", "juce-data-structures", "juce-events", "juce-graphics", "juce-gui-basics" }
+
+package.config["Debug"].target         = project.name .. "_debug"
+package.config["Debug"].objdir         = "intermediate/Debug"
+package.config["Debug"].defines        = { "DEBUG=1", "_DEBUG=1" }
+package.config["Debug"].buildoptions   = { "-O0 -ggdb -msse" }
+package.config["Debug"].links          = { "juce-core_debug", "juce-audio-basics_debug", "juce-audio-devices_debug", "juce-audio-formats_debug", "juce-audio-processors_debug", "juce-audio-utils_debug", "juce-data-structures_debug", "juce-events_debug", "juce-graphics_debug", "juce-gui-basics_debug" }
 
 if (windows) then
-  package.defines = { package.defines, "WINDOWS=1" }
+  package.defines      = { package.defines, "WINDOWS=1", "NO_OMP" }
+  package.buildoptions = { package.buildoptions, "-fpermissive -std=c++0x" }
+  package.links        = { "gdi32", "imm32", "ole32", "shlwapi", "uuid", "version", "wininet", "ws2_32" }
+elseif (macosx) then
+  package.defines      = { package.defines, "MAC=1" }
+  package.buildoptions = { package.buildoptions, "-ObjC++" }
+  package.linkoptions  = { package.linkoptions, "-bundle -framework Carbon -framework Cocoa -framework QuartzCore" }
 else
-  package.config["Release"].buildoptions = { "-O2 -mtune=generic -msse -ffast-math -fomit-frame-pointer -funroll-loops -fopenmp -fvisibility=hidden -fPIC" }
-  package.config["Debug"].buildoptions   = { "-O0 -ggdb -msse -fPIC" }
-  if (macosx) then
-    package.defines = { package.defines, "MAC=1" }
-    package.targetextension = "dylib"
-    package.buildoptions    = { "-ObjC++" }
-    package.linkoptions     = { "-bundle -framework Carbon -framework Cocoa -framework QuartzCore" }
-  else
-    package.defines = { package.defines, "LINUX=1" }
-    package.links   = { "freetype", "pthread", "rt", "X11", "Xext", "gomp" }
-    package.buildoptions = { "`pkg-config --cflags freetype2 fftw3f`" }
-    package.linkoptions  = { "`pkg-config --libs freetype2 fftw3f`" }
-  end
+  package.defines      = { package.defines, "LINUX=1" }
+  package.buildoptions = { package.buildoptions, "-std=c++0x" }
+  package.links        = { "pthread", "rt", "X11", "Xext", "gomp" }
+  package.linkoptions  = { package.linkoptions, "`pkg-config --libs freetype2`" }
 end
 
 package.includepaths = {
--- ports/wolpertinger/VST/premake.lua
+++ ports/wolpertinger/VST/premake.lua
@@ -13,33 +13,33 @@ package.targetprefix = ""
 package.defines   = { "JucePlugin_Build_AU=0", "JucePlugin_Build_LV2=0", "JucePlugin_Build_RTAS=0", "JucePlugin_Build_VST=1", "JucePlugin_Build_Standalone=0", "BUILDDATE=\"`date +%F`\"", "WOLPVERSION=\"0041\"", "WOLPVERSIONSTRING=\"0.4.1\"" }
 package.linkflags = { "no-symbols", "static-runtime" }
 
-package.config["Release"].target     = project.name
-package.config["Release"].objdir     = "intermediate/Release"
-package.config["Release"].defines    = { "NDEBUG=1", "CONFIGURATION=\"Release\"" }
-package.config["Release"].buildflags = { "no-symbols", "optimize-speed" }
-package.config["Release"].links      = { "juce-core", "juce-audio-basics", "juce-audio-devices", "juce-audio-formats", "juce-audio-processors", "juce-audio-utils", "juce-data-structures", "juce-events", "juce-graphics", "juce-gui-basics" }
+package.config["Release"].target       = project.name
+package.config["Release"].objdir       = "intermediate/Release"
+package.config["Release"].defines      = { "NDEBUG=1", "CONFIGURATION=\"Release\"" }
+package.config["Release"].buildflags   = { "no-symbols", "optimize-speed" }
+package.config["Release"].buildoptions = { "-O2 -ffast-math -fomit-frame-pointer -fvisibility=hidden -mtune=generic -msse -mfpmath=sse" }
+package.config["Release"].links        = { "juce-core", "juce-audio-basics", "juce-audio-devices", "juce-audio-formats", "juce-audio-processors", "juce-audio-utils", "juce-data-structures", "juce-events", "juce-graphics", "juce-gui-basics" }
 
-package.config["Debug"].target       = project.name .. "_debug"
-package.config["Debug"].objdir       = "intermediate/Debug"
-package.config["Debug"].defines      = { "DEBUG=1", "_DEBUG=1", "CONFIGURATION=\"Debug\"" }
-package.config["Debug"].links        = { "juce-core_debug", "juce-audio-basics_debug", "juce-audio-devices_debug", "juce-audio-formats_debug", "juce-audio-processors_debug", "juce-audio-utils_debug", "juce-data-structures_debug", "juce-events_debug", "juce-graphics_debug", "juce-gui-basics_debug" }
+package.config["Debug"].target         = project.name .. "_debug"
+package.config["Debug"].objdir         = "intermediate/Debug"
+package.config["Debug"].defines        = { "DEBUG=1", "_DEBUG=1", "CONFIGURATION=\"Debug\"" }
+package.config["Debug"].buildoptions   = { "-O0 -ggdb" }
+package.config["Debug"].links          = { "juce-core_debug", "juce-audio-basics_debug", "juce-audio-devices_debug", "juce-audio-formats_debug", "juce-audio-processors_debug", "juce-audio-utils_debug", "juce-data-structures_debug", "juce-events_debug", "juce-graphics_debug", "juce-gui-basics_debug" }
 
 if (windows) then
-  package.defines = { package.defines, "WINDOWS=1", "BINTYPE=\"Windows-VST\"" }
+  package.defines      = { package.defines, "WINDOWS=1", "BINTYPE=\"Windows-VST\"" }
+  package.buildoptions = { "-fpermissive -std=c++0x" }
+  package.links        = { "gdi32", "imm32", "ole32", "shlwapi", "uuid", "version", "wininet", "ws2_32" }
+elseif (macosx) then
+  package.defines      = { package.defines, "MAC=1", "BINTYPE=\"Mac-VST\"" }
+  package.targetextension = "dylib"
+  package.buildoptions = { "-ObjC++" }
+  package.linkoptions  = { "-bundle -framework Carbon -framework Cocoa -framework QuartzCore" }
 else
-  package.config["Release"].buildoptions = { "-O2 -mtune=generic -msse -ffast-math -fomit-frame-pointer -fvisibility=hidden -fPIC" }
-  package.config["Debug"].buildoptions   = { "-O0 -ggdb -fPIC" }
-  if (macosx) then
-    package.defines = { package.defines, "MAC=1", "BINTYPE=\"Mac-VST\"" }
-    package.targetextension = "dylib"
-    package.buildoptions    = { "-ObjC++" }
-    package.linkoptions     = { "-bundle -framework Carbon -framework Cocoa -framework QuartzCore" }
-  else
-    package.defines = { package.defines, "LINUX=1", "BINTYPE=\"Linux-VST\"" }
-    package.links   = { "freetype", "pthread", "rt", "m", "X11", "Xext" }
-    package.buildoptions = { "`pkg-config --cflags freetype2`" }
-    package.linkoptions  = { "`pkg-config --libs freetype2`" }
-  end
+  package.defines      = { package.defines, "LINUX=1", "BINTYPE=\"Linux-VST\"" }
+  package.buildoptions = { "-std=c++0x" }
+  package.links        = { "pthread", "rt", "m", "X11", "Xext" }
+  package.linkoptions  = { "`pkg-config --libs freetype2`" }
 end
 
 package.includepaths = {
